8f227ce893eb53232958fa4fd87a1ae15db09e05
muhammadmasud1 <mdmasudrana15020@gmail.com>
2023-12-01 15:44:54 +0600

    showModal

--- watch.js
+++ watch.js
@@ -49,32 +49,53 @@ const watchProduct = [
   },
 ];
 
-function displayProduct() {
+function displayProduct(showMorePdt,modal) {
   const watchProductId = document.getElementById("watchProduct");
-  for (let product of watchProduct) {
+  showMorePdt = watchProduct.slice(0, 4); // Assuming watchProduct is defined elsewhere
+  for (let product of showMorePdt) {
     let div = document.createElement("div");
     div.innerHTML = `
-              <div class="card w-ful h-full bg-base-100 shadow-xl rounded-lg overflow-hidden">
-            <figure><img class="w-full h-full rounded-sm data-te-ripple-init
-            border hover:scale-105 transition cursor-pointer" src="${product.productImage}" alt="watch" /></figure>
-            <div class="card-body text-center p-0 m-2">
-              <h2 class="card-title text-center text-xl mx-auto">${product.productName}</h2>
-              <h4 class="card-title text-center  mx-auto">${product.ProductPrice}</h4>
-              </div>
-              <div class="text-center my-5 flex justify-around">
-              <button class="btn btn-outline btn-secondary btn-sm" onclick="addtocart(this)">Add Cart</button>
-              <button class="btn btn-outline btn-secondary btn-sm">Details</button>
-            </div> 
-            </div>
-             </div>      
-              `;
-              
+      <div class="card w-ful h-full bg-base-100 shadow-xl rounded-lg overflow-hidden">
+        <figure><img class="w-full h-full rounded-sm data-te-ripple-init
+          border hover:scale-105 transition cursor-pointer" src="${product.productImage}" alt="watch" /></figure>
+        <div class="card-body text-center p-0 m-2">
+          <h2 class="card-title text-center text-xl mx-auto">${product.productName}</h2>
+          <h4 class="card-title text-center mx-auto">${product.ProductPrice}</h4>
+        </div>
+        <div class="text-center my-5 flex justify-around">
+          <button class="btn btn-outline btn-secondary btn-sm" onclick="addtocart(this)">Add Cart</button>
+          <button id="btnShowModal" class="btn btn-outline btn-secondary btn-sm" onclick="showModal()">Details</button>
+        </div> 
+      </div>
+    </div>`;
+
     watchProductId.appendChild(div);
   }
 }
-displayProduct();
+const showModal = (product,modal) => {
+  const showModalId = document.getElementById('modalBox');
+  showModalId.innerHTML = `
+    <img src="${product.productImage}" class="text-center mx-auto">
+    <h3 class="font-bold text-lg text-center">${product.productName}</h3>
+    <p class="font-bold text-lg text-center">$ 10</p>
+    <div class="modal-action">
+      <form method="dialog">
+        <button class="btn">Close</button>
+      </form>
+    </div>`;
+};
+const productsToShow = watchProduct;
+displayProduct(productsToShow);
 
-function addtocart(event) {
+document.getElementById('btnShowModal').addEventListener('click', function() {
+  showModal(productsToShow[0]); 
+  my_modal_1.showModal();
+});
+
+
+
+
+function addtocart(event,showMorePdt) {
   const cartAppend = document.getElementById("cartAppend");
   /* console.log(event.parentNode.parentNode.childNodes[1].childNodes[0].src); 
 console.log(event.parentNode.parentNode.childNodes[3].childNodes[1]);
@@ -98,3 +119,8 @@ console.log(event.parentNode.parentNode.childNodes[3].childNodes[3]); */
 cartAppend.appendChild(div)
 }
 addtocart();
+
+function showMorePdt(showMorePdt){
+  displayProduct(true)
+}
+
